2f4fc78ddfaf199b336b43f55409c43c48ab61f9
João Paulo Faria <jpfaria@gmail.com>
2024-07-01 16:05:47 -0300

    wip: initial boostrap impl

--- factory/contrib/go-resty/resty/v2/examples/cobra/main.go
+++ factory/contrib/go-resty/resty/v2/examples/cobra/main.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"github.com/xgodev/boost"
 	log2 "github.com/xgodev/boost/factory/contrib/go-resty/resty/v2/plugins/local/wrapper/log"
+	c "github.com/xgodev/boost/factory/contrib/spf13/cobra/v1"
 	"os"
 
 	r "github.com/go-resty/resty/v2"
 	"github.com/spf13/cobra"
 	"github.com/xgodev/boost/factory/contrib/go-resty/resty/v2"
-	c "github.com/xgodev/boost/factory/contrib/spf13/v1"
 	ilog "github.com/xgodev/boost/factory/local/wrapper/log"
 	"github.com/xgodev/boost/wrapper/log"
 )
