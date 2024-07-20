c1a476bdd0c20c16da13ac6f2e254a4cf38be850
João Paulo Faria <jpfaria@gmail.com>
2024-07-19 22:55:29 -0300

    feat: adds plugins on cloudevents http boostrap function

--- fx/modules/local/bootstrap/function/adapter/contrib/cloudevents/sdk-go/v2/core/http/module.go
+++ fx/modules/local/bootstrap/function/adapter/contrib/cloudevents/sdk-go/v2/core/http/module.go
@@ -2,12 +2,18 @@ package http
 
 import (
 	ce "github.com/cloudevents/sdk-go/v2"
+	"github.com/cloudevents/sdk-go/v2/client"
 	fn "github.com/xgodev/boost/bootstrap/function"
 	"github.com/xgodev/boost/bootstrap/function/adapter/contrib/cloudevents/sdk-go/v2/core/http"
 	"github.com/xgodev/boost/fx/modules/local/bootstrap/function"
 	"go.uber.org/fx"
 	"sync"
 )
+
+type params struct {
+	fx.In
+	Plugins []http.Plugin `optional:"true"`
+}
 
 var once sync.Once
 
@@ -22,10 +28,13 @@ func Module() fx.Option {
 			fx.Provide(
 				fx.Annotated{
 					Group: function.BSFunctionAdaptersGroupKey,
-					Target: func() fn.CmdFunc {
+					Target: func(p params) fn.CmdFunc {
 						return http.New(
-							ce.WithUUIDs(),
-							ce.WithTimeNow(),
+							[]client.Option{
+								ce.WithUUIDs(),
+								ce.WithTimeNow(),
+							},
+							p.Plugins...,
 						)
 					},
 				},
