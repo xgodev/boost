29e2d48a960c9517049da3135cfd3e24dd6b3815
João Paulo Faria <jpfaria@gmail.com>
2024-07-19 22:37:32 -0300

    feat: adds initial dapr impl

---
+++ factory/contrib/dapr/go-sdk/v1/client/options.go
@@ -0,0 +1,20 @@
+package client
+
+import (
+	"github.com/xgodev/boost/wrapper/config"
+)
+
+// Options represents cache options.
+type Options struct {
+	Address string
+}
+
+// NewOptions returns options from config file or environment vars.
+func NewOptions() (*Options, error) {
+	return config.NewOptionsWithPath[Options](root)
+}
+
+// NewOptionsWithPath unmarshals a given key path into options and returns it.
+func NewOptionsWithPath(path string) (opts *Options, err error) {
+	return config.NewOptionsWithPath[Options](root, path)
+}
