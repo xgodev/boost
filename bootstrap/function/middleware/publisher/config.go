376a9d9d6cd1f8ef824eaa67c001578d0b1d27b8
João Paulo Faria <jpfaria@gmail.com>
2024-08-11 18:48:32 -0300

    Add new configurations for dead letter and retry settings
    Expanded configuration options for dead letter and retry functionalities in the publisher middleware. New settings include enabling dead letter, defining its subject and errors list, as well as enabling retry with a specified backoff value.

--- bootstrap/function/middleware/publisher/config.go
+++ bootstrap/function/middleware/publisher/config.go
@@ -6,10 +6,22 @@ import (
 )
 
 const (
-	root    = middleware.Root + ".publisher"
-	subject = root + ".subject"
+	root       = middleware.Root + ".publisher"
+	subject    = root + ".subject"
+	deadletter = root + ".deadletter"
+	dlenabled  = deadletter + ".enabled"
+	dlsubject  = deadletter + ".subject"
+	dlerrors   = deadletter + ".errors"
+	retry      = root + ".retry"
+	renabled   = retry + ".enabled"
+	rbackoff   = retry + ".backoff"
 )
 
 func init() {
 	config.Add(subject, "changeme", "defines output subject")
+	config.Add(dlenabled, false, "enables dead letter")
+	config.Add(dlsubject, "changeme", "defines dead letter output subject")
+	config.Add(dlerrors, []error{}, "defines dead letter errors list")
+	config.Add(renabled, false, "enables retry")
+	config.Add(rbackoff, 3, "defines retry backoff")
 }
