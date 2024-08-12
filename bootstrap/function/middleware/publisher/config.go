40c97062659cc8bf149d8b3f53863b074571795c
João Paulo Faria <jpfaria@gmail.com>
2024-08-12 17:48:31 -0300

    Add error handling options for dead letter and retry in publisher middleware
    - Added error handling options for dead letter and retry in the publisher middleware configuration.
    - Updated the config settings to include error lists for dead letter and retry scenarios.

--- bootstrap/function/middleware/publisher/config.go
+++ bootstrap/function/middleware/publisher/config.go
@@ -2,6 +2,7 @@ package publisher
 
 import (
 	"github.com/xgodev/boost/bootstrap/function/middleware"
+	"github.com/xgodev/boost/model/errors"
 	"github.com/xgodev/boost/wrapper/config"
 )
 
@@ -15,13 +16,15 @@ const (
 	retry      = root + ".retry"
 	renabled   = retry + ".enabled"
 	rbackoff   = retry + ".backoff"
+	rerrors    = retry + ".errors"
 )
 
 func init() {
 	config.Add(subject, "changeme", "defines output subject")
 	config.Add(dlenabled, false, "enables dead letter")
 	config.Add(dlsubject, "changeme", "defines dead letter output subject")
-	config.Add(dlerrors, []error{}, "defines dead letter errors list")
+	config.Add(dlerrors, []error{errors.Internalf("")}, "defines dead letter errors list")
 	config.Add(renabled, false, "enables retry")
 	config.Add(rbackoff, 3, "defines retry backoff")
+	config.Add(rerrors, []error{errors.ServiceUnavailablef("")}, "defines retry errors list")
 }
