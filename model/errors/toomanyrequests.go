88cca834faf2556bf0e9c3abac8c7ef674f21278
Gabriel Z Amaral <gabriel.zuqueto@gmail.com>
2024-08-14 03:11:09 -0300

    Update toomanyrequests.go
    Fix functions names

--- model/errors/toomanyrequests.go
+++ model/errors/toomanyrequests.go
@@ -1,26 +1,26 @@
 package errors
 
-// toManyRequests represents an error when a request cannot be completed because of
+// tooManyRequests represents an error when a request cannot be completed because of
 // sent too many requests in a given amount of  time
-type toManyRequests struct {
+type tooManyRequests struct {
 	Err
 }
 
-// ToManyRequestsf returns an error which satistifes IsToManyRequests()
-func ToManyRequestsf(format string, args ...interface{}) error {
-	return &toManyRequests{wrap(nil, format, "", args...)}
+// TooManyRequestsf returns an error which satistifes IsTooManyRequests()
+func TooManyRequestsf(format string, args ...interface{}) error {
+	return &tooManyRequests{wrap(nil, format, "", args...)}
 }
 
-// NewToManyRequests returns an error which wraps err that satisfies
-// IsToManyRequests().
-func NewToManyRequests(err error, msg string) error {
-	return &toManyRequests{wrap(err, msg, "")}
+// NewTooManyRequests returns an error which wraps err that satisfies
+// IsTooManyRequests().
+func NewTooManyRequests(err error, msg string) error {
+	return &tooManyRequests{wrap(err, msg, "")}
 }
 
-// IsToManyRequests reports whether err was created with ToManyRequestsf() or
-// NewInternal().
-func IsToManyRequests(err error) bool {
+// IsTooManyRequests reports whether err was created with TooManyRequestsf() or
+// NewTooManyRequests().
+func IsTooManyRequests(err error) bool {
 	err = Cause(err)
-	_, ok := err.(*toManyRequests)
+	_, ok := err.(*tooManyRequests)
 	return ok
 }
