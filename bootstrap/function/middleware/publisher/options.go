5d8989f97ce2742ea0e03d5b9186c2fd7174463d
João Paulo Faria <jpfaria@gmail.com>
2024-08-20 01:39:11 -0300

    Refactor dead letter error handling and options struct
    - Updated error handling for dead letter messages
    - Refactored options struct to include DeadletterOptions

--- bootstrap/function/middleware/publisher/options.go
+++ bootstrap/function/middleware/publisher/options.go
@@ -4,18 +4,15 @@ import (
 	"github.com/xgodev/boost/wrapper/config"
 )
 
+type DeadletterOptions struct {
+	Enabled bool
+	Subject string
+	Errors  []string
+}
+
 type Options struct {
 	Subject    string
-	Deadletter struct {
-		Enabled bool
-		Subject string
-		Errors  []error
-	}
-	Retry struct {
-		Enabled bool
-		Backoff int
-		Errors  []error
-	}
+	Deadletter DeadletterOptions
 }
 
 func NewOptions() (*Options, error) {
--- bootstrap/function/middleware/publisher/publisher.go
+++ bootstrap/function/middleware/publisher/publisher.go
@@ -3,7 +3,9 @@ package publisher
 import (
 	"github.com/cloudevents/sdk-go/v2/event"
 	"github.com/xgodev/boost/extra/middleware"
+	berrors "github.com/xgodev/boost/model/errors"
 	"github.com/xgodev/boost/wrapper/publisher"
+	"reflect"
 )
 
 type Publisher[T any] struct {
@@ -12,32 +14,56 @@ type Publisher[T any] struct {
 }
 
 func (c *Publisher[T]) Exec(ctx *middleware.AnyErrorContext[T], exec middleware.AnyErrorExecFunc[T], fallbackFunc middleware.AnyErrorReturnFunc[T]) (T, error) {
+
 	e, err := ctx.Next(exec, fallbackFunc)
+	if &e != nil {
 
-	var events []*event.Event
+		var events []*event.Event
 
-	switch r := any(e).(type) {
-	case []*event.Event:
-		events = r
-	case *event.Event:
-		if r != nil {
+		switch r := any(e).(type) {
+		case []*event.Event:
+			events = r
+		case *event.Event:
 			events = []*event.Event{r}
+		default:
+			return e, berrors.Internalf("unsupported handler type")
 		}
-	default:
-		return e, err
+
+		var deadLetterSubject string
+		var errorType string
+
+		if err != nil && c.options.Deadletter.Enabled {
+
+			errType := reflect.TypeOf(err).Elem().Name()
+
+			for _, allowedErrorType := range c.options.Deadletter.Errors {
+				if errType == allowedErrorType {
+					deadLetterSubject = c.options.Deadletter.Subject
+					errorType = errType
+					break
+				}
+			}
+
+		}
+
+		if err != nil && deadLetterSubject == "" {
+			return e, err
+		}
+
+		for _, ev := range events {
+
+			if deadLetterSubject != "" {
+				ev.SetSubject(deadLetterSubject)
+				ev.SetExtension("error_type", errorType)
+				ev.SetExtension("error", err.Error())
+			} else if ev.Subject() == "" {
+				ev.SetSubject(c.options.Subject)
+			}
+		}
+
+		return e, c.publisher.Publish(ctx.GetContext(), events)
 	}
-
-	for _, ev := range events {
-		if ev == nil {
-			continue
-		}
-		if ev.Subject() == "" {
-			ev.SetSubject(c.options.Subject)
-		}
-	}
-
-	return e, c.publisher.Publish(ctx.GetContext(), events)
-
+	return e, err
 }
 
 func NewAnyErrorMiddleware[T any](publisher *publisher.Publisher) (middleware.AnyErrorMiddleware[T], error) {
