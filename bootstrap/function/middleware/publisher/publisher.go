80499fb9214cdf754b0a28b7cc15417d74b3ff49
João Paulo Faria <jpfaria@gmail.com>
2024-09-21 01:16:33 -0300

    Refactor event handling and dead letter logic
    - Refactored event handling to handle different types efficiently
    - Improved dead letter error type identification and logging
    - Added checks for empty events before publishing

--- bootstrap/function/middleware/publisher/publisher.go
+++ bootstrap/function/middleware/publisher/publisher.go
@@ -20,75 +20,81 @@ func (c *Publisher[T]) Exec(ctx *middleware.AnyErrorContext[T], exec middleware.
 	logger := log.FromContext(ctx.GetContext())
 
 	e, err := ctx.Next(exec, fallbackFunc)
-	if &e != nil {
 
-		var events []*event.Event
+	var events []*event.Event
 
-		switch r := any(e).(type) {
-		case []*event.Event:
-			events = r
-		case *event.Event:
-			events = []*event.Event{r}
-		default:
-			return e, errors.Internalf("unsupported handler type")
-		}
+	switch r := any(e).(type) {
+	case []*event.Event:
+		events = r
+	case *event.Event:
+		events = []*event.Event{r}
+	case nil:
+		events = []*event.Event{}
+	default:
+		return e, errors.Internalf("unsupported handler type")
+	}
 
-		var deadLetterSubject string
-		var errorType string
+	var deadLetterSubject string
+	var errorType string
 
-		if err != nil {
+	if err != nil {
 
-			if c.options.Deadletter.Enabled {
+		if c.options.Deadletter.Enabled {
 
-				err = errors.Cause(err)
+			err = errors.Cause(err)
 
-				errType := reflect.TypeOf(err).Elem().Name()
+			errType := reflect.TypeOf(err).Elem().Name()
 
-				logger.Debugf("configured to send to deadletter error types: [%s]", strings.Join(c.options.Deadletter.Errors, ", "))
-				logger.Warnf("contains error type %s. %s",
-					errType,
-					err.Error())
+			logger.Debugf("configured to send to deadletter error types: [%s]", strings.Join(c.options.Deadletter.Errors, ", "))
+			logger.Warnf("contains error type %s. %s",
+				errType,
+				err.Error())
 
-				for _, allowedErrorType := range c.options.Deadletter.Errors {
-					if errType == allowedErrorType {
-						logger.Tracef("Error type %s is allowed to be sent to dead letter", errType)
-						deadLetterSubject = c.options.Deadletter.Subject
-						errorType = errType
-						break
-					}
+			for _, allowedErrorType := range c.options.Deadletter.Errors {
+				if errType == allowedErrorType {
+					logger.Tracef("Error type %s is allowed to be sent to dead letter", errType)
+					deadLetterSubject = c.options.Deadletter.Subject
+					errorType = errType
+					break
 				}
-
-				if deadLetterSubject == "" {
-					logger.Warnf("no dead letter subject found for error type %s", errType)
-					return e, nil
-				}
-
-				for _, ev := range events {
-					ev.SetSubject(deadLetterSubject)
-					ev.SetExtension("error_type", errorType)
-					ev.SetExtension("error", err.Error())
-				}
-
 			}
 
-		}
-
-		for _, ev := range events {
-			if ev.Subject() == "" {
-				if c.options.Subject == "" {
-					logger.Warnf("no subject found for event. ignoring publish")
-					return e, nil
-				}
-				ev.SetSubject(c.options.Subject)
+			if deadLetterSubject == "" {
+				logger.Warnf("no dead letter subject found for error type %s", errType)
+				return e, nil
 			}
+
+			for _, ev := range events {
+				ev.SetSubject(deadLetterSubject)
+				ev.SetExtension("error_type", errorType)
+				ev.SetExtension("error", err.Error())
+			}
+
+		} else {
+			logger.Debugf("dead letter is disabled. ignoring dead letter")
 		}
 
-		if err := c.publisher.Publish(ctx.GetContext(), events); err != nil {
-			return e, err
-		}
+	}
 
+	for _, ev := range events {
+		if ev.Subject() == "" {
+			if c.options.Subject == "" {
+				logger.Warnf("no subject found for event. ignoring publish")
+				return e, nil
+			}
+			ev.SetSubject(c.options.Subject)
+		}
+	}
+	
+	if len(events) == 0 {
+		logger.Debugf("no events to publish")
+		return e, nil
+	}
+
+	if err := c.publisher.Publish(ctx.GetContext(), events); err != nil {
 		return e, err
 	}
+
 	return e, err
 }
 
