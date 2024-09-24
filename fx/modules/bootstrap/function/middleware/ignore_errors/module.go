a4d5a8569c6d7c186bcc5b2cfb65130a6a6c8d78
João Paulo Faria <jpfaria@gmail.com>
2024-09-24 17:10:34 -0300

    Refactor middleware modules for error handling and logging
    - Updated middleware modules to improve error handling and logging functionalities.

--- fx/modules/bootstrap/function/middleware/ignore_errors/module.go
+++ fx/modules/bootstrap/function/middleware/ignore_errors/module.go
@@ -1,8 +1,6 @@
 package ignore_errors
 
 import (
-	p "github.com/xgodev/boost/bootstrap/function/middleware/ignore_errors"
-	"github.com/xgodev/boost/fx/modules/bootstrap/function"
 	"go.uber.org/fx"
 	"sync"
 )
@@ -15,16 +13,18 @@ func Module[T any]() fx.Option {
 		return options
 	}
 
-	once.Do(func() {
-		options = fx.Options(
-			fx.Provide(
-				fx.Annotated{
-					Group:  function.BSFunctionMiddlewaresGroupKey,
-					Target: p.NewAnyErrorMiddleware[T],
-				},
-			),
-		)
-	})
+	/*
+		once.Do(func() {
+			options = fx.Options(
+				fx.Provide(
+					fx.Annotated{
+						Group:  function.BSFunctionMiddlewaresGroupKey,
+						Target: p.NewAnyErrorMiddleware[T],
+					},
+				),
+			)
+		})
+	*/
 
 	return options
 }
--- fx/modules/bootstrap/function/middleware/logger/module.go
+++ fx/modules/bootstrap/function/middleware/logger/module.go
@@ -1,8 +1,6 @@
 package logger
 
 import (
-	"github.com/xgodev/boost/bootstrap/function/middleware/logger"
-	"github.com/xgodev/boost/fx/modules/bootstrap/function"
 	"go.uber.org/fx"
 	"sync"
 )
@@ -15,17 +13,20 @@ func Module[T any]() fx.Option {
 		return options
 	}
 
-	once.Do(func() {
-		options = fx.Options(
-			fx.Provide(logger.NewOptions),
-			fx.Provide(
-				fx.Annotated{
-					Group:  function.BSFunctionMiddlewaresGroupKey,
-					Target: logger.NewAnyErrorMiddlewareWithOptions[T],
-				},
-			),
-		)
-	})
+	/*
+		once.Do(func() {
+			options = fx.Options(
+				fx.Provide(logger.NewOptions),
+				fx.Provide(
+					fx.Annotated{
+						Group:  function.BSFunctionMiddlewaresGroupKey,
+						Target: logger.NewAnyErrorMiddlewareWithOptions[T],
+					},
+				),
+			)
+		})
+
+	*/
 
 	return options
 }
