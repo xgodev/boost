fb9bd0e7e717686aa7a9e833437aae013dffaff7
João Paulo Faria <jpfaria@gmail.com>
2025-02-06 17:25:18 -0300

    Add AppConfig struct and environment variable handling
    - Added AppConfig struct for application configuration.
    - Initialized and loaded environment variables for the application name.
    - Unmarshaled and logged application name from different environment settings.

---
+++ wrapper/config/examples/env/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/xgodev/boost"
+	"github.com/xgodev/boost/wrapper/config"
+	"log"
+	"os"
+)
+
+type AppConfig struct {
+	Application struct {
+		Name string
+	}
+}
+
+func init() {
+	config.Add("app.application.name", "app_test", "name of application")
+}
+
+func main() {
+
+	boost.Start()
+
+	c1 := AppConfig{}
+	config.UnmarshalWithPath("app", &c1)
+
+	log.Printf(c1.Application.Name)
+
+	os.Setenv("APP_APPLICATION_NAME", "app_test_env_2")
+	config.Load()
+
+	c2 := AppConfig{}
+	config.UnmarshalWithPath("app", &c2)
+
+	log.Printf(c2.Application.Name)
+
+	os.Setenv("APP_APPLICATION_NAME", "app_test_env_3")
+	config.Load()
+
+	c3 := AppConfig{}
+	config.UnmarshalWithPath("app", &c3)
+
+	log.Printf(c3.Application.Name)
+
+}
