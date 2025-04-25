ebcc9f025a5512255cffdf53380e93c474892fca
João Paulo Faria <jpfaria@gmail.com>
2025-04-25 03:01:55 -0300

    Adds shared API-level GCP configuration
    Introduces shared API-level GCP client configuration, including options for project ID, credentials, endpoint overrides, emulator usage, user-agent, scopes, timeout, proxy settings, and retry policies.
    
    Provides a mechanism to apply these options when creating Google API clients.

---
+++ factory/contrib/cloud.google.com/api/v0/api.go
@@ -0,0 +1,52 @@
+package api
+
+import (
+	"context"
+	"google.golang.org/api/option"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+// ApplyAPIOptions retorna os option.ClientOption da biblioteca
+// google.golang.org/api baseados em Options.
+func ApplyAPIOptions(ctx context.Context, o *Options) []option.ClientOption {
+
+	var opts []option.ClientOption
+	// proxy
+	if o.Proxy != "" {
+		if u, err := url.Parse(o.Proxy); err == nil {
+			httpc := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(u)}}
+			opts = append(opts, option.WithHTTPClient(httpc))
+		}
+	}
+	// emulator
+	if o.UseEmulator {
+		host := o.EmulatorHost
+		if host == "" {
+			host = os.Getenv("EMULATOR_HOST")
+		}
+		opts = append(opts, option.WithEndpoint(host), option.WithoutAuthentication())
+	} else {
+		// credentials
+		if o.Credentials.JSON != "" {
+			opts = append(opts, option.WithCredentialsJSON([]byte(o.Credentials.JSON)))
+		} else {
+			opts = append(opts, option.WithCredentialsFile(o.Credentials.File))
+		}
+	}
+	// endpoint, scopes, user-agent
+	if o.Endpoint != "" {
+		opts = append(opts, option.WithEndpoint(o.Endpoint))
+	}
+	if len(o.ParseScopes()) > 0 {
+		opts = append(opts, option.WithScopes(o.ParseScopes()...))
+	}
+	if o.UserAgent != "" {
+		opts = append(opts, option.WithUserAgent(o.UserAgent))
+	}
+
+	// opts = append(opts, option.WithLogger(log.GetLogger()))
+
+	return opts
+}
