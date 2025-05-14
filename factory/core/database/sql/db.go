9e0f79d01605be10f7929f349f8fa2acf50d368a
João Paulo Faria <jpfaria@gmail.com>
2025-05-14 00:14:16 -0300

    Adiciona opções configuráveis para SQL
    Define parâmetros de conexão como tempo de vida e limites de conexões,
    permitindo ajustes via configurações. Atualiza a criação do banco para
    utilizar as novas opções e remove implementações antigas.

--- factory/core/database/sql/db.go
+++ factory/core/database/sql/db.go
@@ -7,12 +7,26 @@ import (
 	"github.com/xgodev/boost/wrapper/log"
 )
 
-// NewDB returns a new sql DB.
-func NewDB(ctx context.Context, connector driver.Connector, plugins ...Plugin) (db *sql.DB, err error) {
+// NewDBWithConfigPath returns a new sql DB with options from config path.
+func NewDBWithConfigPath(ctx context.Context, connector driver.Connector, path string, plugins ...Plugin) (*sql.DB, error) {
+	opts, err := NewOptionsWithPath(path)
+	if err != nil {
+		return nil, err
+	}
+	return NewDBWithOptions(ctx, connector, opts, plugins...)
+}
+
+// NewDBWithOptions returns a new sql DB.
+func NewDBWithOptions(ctx context.Context, connector driver.Connector, options *Options, plugins ...Plugin) (db *sql.DB, err error) {
 
 	logger := log.FromContext(ctx)
 
 	db = sql.OpenDB(connector)
+
+	db.SetConnMaxLifetime(options.ConnMaxLifetime)
+	db.SetConnMaxIdleTime(options.ConnMaxIdletime)
+	db.SetMaxIdleConns(options.MaxIdletime)
+	db.SetMaxOpenConns(options.MaxOpenConns)
 
 	for _, plugin := range plugins {
 		db, err = plugin(ctx, db, connector)
@@ -25,7 +39,20 @@ func NewDB(ctx context.Context, connector driver.Connector, plugins ...Plugin) (
 		return nil, err
 	}
 
-	logger.Info("Connected to Oracle (godror) server")
+	logger.Info("Connected to sql connector driver")
 
 	return db, err
+}
+
+// NewDB returns a new DB.
+func NewDB(ctx context.Context, connector driver.Connector, plugins ...Plugin) (*sql.DB, error) {
+
+	logger := log.FromContext(ctx)
+
+	o, err := NewOptions()
+	if err != nil {
+		logger.Fatalf(err.Error())
+	}
+
+	return NewDBWithOptions(ctx, connector, o, plugins...)
 }
